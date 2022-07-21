12e9edbf97b5f3f50d51946c1f9bc1f173a648b6
Ricky Martinez <RMartinezW16@gmail.com>
2022-07-21 13:20:58 -0400

    more style

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./main.css";
 
 function Main(){
     return (
-        <div>
-            <h1>Fun facts about React</h1>
-                <ul>
+        <div className="container">
+            <h1 className="header">Fun facts about React</h1>
+                <ul className="list">
                     <li>Was first released in 2013</li>
                     <li>Was originally created by Jordan Walke</li>
                     <li>Has well over 100k stars on GitHub</li>
