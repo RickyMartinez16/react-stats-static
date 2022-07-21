ca15b7a6837f23c0af8ce3eecaac5c325e611dfd
Ricky Martinez <RMartinezW16@gmail.com>
2022-07-21 12:33:29 -0400

    nav styled

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,14 @@
 import React from "react";
 import logo from "./react-logo.png"
+import "./nav.css"
 
 function Nav(){
     return (
-        <div>
-            <img src={logo} width="40px" alt="react-logo" />
-            <h2>ReactFacts</h2>
-            <h5>React Course - Project 1</h5>
-        </div>
+        <nav>
+            <img src={logo} width="40px" alt="react-logo" className="main-logo"/>
+            <h2 className="logo-text">ReactFacts</h2>
+            <h4 className="nav-title">React Course - Project 1</h4>
+        </nav>
     )
 }
 
